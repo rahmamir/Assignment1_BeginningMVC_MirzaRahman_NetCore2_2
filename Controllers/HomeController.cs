320697fe9f383f1163687bbf0b1cf0ac1783d307
Mirza Rahman <galeeb942010@hotmail.com>
2019-10-20 10:42:20 -0400

    Milestone 2 : Implemented Basic Home Page, will improve with better UI and graphical look

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1_BeginningMVC_MirzaRahman_NetCore2_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "FAST Equipment Tool";
+            return View("HomePage");
+        }
+    }
+}
